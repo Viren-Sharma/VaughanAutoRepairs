2273c4f343f5e179ac5e27fab09f7041b2b73245
virensharma <viren.b.sharma@gmail.com>
2018-08-13 00:49:53 -0400

    Landing page

--- tabs.js
+++ tabs.js
@@ -15,6 +15,6 @@ function openTab(evt, tabName) {
     }
 
     // Show the current tab, and add an "active" class to the button that opened the tab
-    document.getElementById(cityName).style.display = "block";
+    document.getElementById(tabName).style.display = "block";
     evt.currentTarget.className += " active";
 }
